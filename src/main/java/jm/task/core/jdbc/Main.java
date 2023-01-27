1418ed5b2239019e8acbafe24e58d7aca027949c
Bakulin Vladimir <wal1984@mail.ru>
2023-01-28 03:40:52 +0700

    third commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -2,21 +2,22 @@ package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
-        userDaoJDBC.createUsersTable();
-        userDaoJDBC.saveUser("Петр", "Иванов", (byte) 34);
-        userDaoJDBC.saveUser("Мария", "Петрова", (byte) 25);
-        userDaoJDBC.saveUser("Виктор", "Васильев", (byte) 29);
-        userDaoJDBC.saveUser("Елена", "Иванова", (byte) 30);
-        List<User> allUsers = userDaoJDBC.getAllUsers();
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Петр", "Иванов", (byte) 34);
+        userService.saveUser("Мария", "Петрова", (byte) 25);
+        userService.saveUser("Виктор", "Васильев", (byte) 29);
+        userService.saveUser("Елена", "Иванова", (byte) 30);
+        List<User> allUsers = userService.getAllUsers();
         allUsers.stream().forEach(System.out::println);
-        userDaoJDBC.cleanUsersTable();
-        userDaoJDBC.dropUsersTable();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
